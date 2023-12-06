3c33254041f8daf50027a85c635680d11bb1020f
ozzy-m <141748365+ozzy-m@users.noreply.github.com>
2023-12-06 12:35:30 -0600

    fgyfu
    fgjf

--- functions.js
+++ functions.js
@@ -6,4 +6,16 @@ document.addEventListener('DOMContentLoaded', function() {
       animationElement.classList.add('show');
     }, 500);
   });
+  document.addEventListener("DOMContentLoaded", function() {
+    const buttons = document.querySelectorAll('.disappear-btn');
   
+    buttons.forEach(button => {
+      button.addEventListener('click', function() {
+        this.style.opacity = '0'; // Fading out the button
+        this.style.pointerEvents = 'none'; // Disable pointer events to prevent clicking
+        setTimeout(() => {
+          this.style.display = 'none'; // Hide the button after the transition
+        }, 500); // Wait for the transition duration before hiding
+      });
+    });
+  });
